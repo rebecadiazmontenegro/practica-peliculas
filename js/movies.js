43c4df176451044bec384ed96354e00162bebbf6
Rebeca D.Montenegro <rebediazmontenegro@gmail.com>
2025-10-12 19:55:01 +0200

    css/done

--- js/movies.js
+++ js/movies.js
@@ -1,61 +1,61 @@
 const movies = [
 {
     titulo: "Regreso al futuro",
-    año: 1985,
+    year: 1985,
     url_foto: "./assets/regreso_al_futuro.jpg",
     genero: "Accion"
 },
 {
     titulo: "King Creole",
-    año: 1958,
+    year: 1958,
     url_foto: "./assets/king_creole.jpg",
     genero: "Músical"
 },
 {
     titulo: "El Diablo viste de Prada",
-    año: 2006,
+    year: 2006,
     url_foto: "./assets/el_diablo_viste_de_prada.jpg",
     genero: "Comedia"
 },
 {
     titulo: "Spider-Man: Across the Spider-Verse",
-    año: 2023,
+    year: 2023,
     url_foto: "./assets/spiderman.jpg",
     genero: "Acción"
 },
 {
     titulo: "La Jungla de Cristal",
-    año: 1988,
+    year: 1988,
     url_foto: "./assets/la_junga_de_cristal.jpg",
     genero: "Acción"
 },
 {
     titulo: "Barbie",
-    año: 2023,
+    year: 2023,
     url_foto: "./assets/barbie.jpg",
     genero: "Comedia"
 },
 {
     titulo: "Que bello es vivir",
-    año: 1948,
+    year: 1948,
     url_foto: "./assets/que_bello_es_vivir.jpg",
     genero: "Infantil/Fantasía"
 },
 {
     titulo: "Los Teleñecos en Cuento de Navidad",
-    año: 1992,
+    year: 1992,
     url_foto: "./assets/cuento_de_navidad.jpg",
     genero: "Musical"
 },
 {
     titulo: "El Diario de Noa",
-    año: 2004,
+    year: 2004,
     url_foto: "./assets/el_diario_de_noa.jpg",
     genero: "Romance"
 },
 {
     titulo: "Lilo & Stitch",
-    año: 2002,
+    year: 2002,
     url_foto: "./assets/lilo_y_stitch_jpg.jpeg",
     genero: "Comedia"
 }
--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
 import movies from "./movies.js" 
 console.log(movies)
+
+const allPeliculas = document.getElementById("allPeliculas");
+console.log(allPeliculas)
+
+let pelicula = ""
+
+for (let i = 0; i < movies.length; i++) {
+    pelicula +=`
+    <article class="cardPelicula">
+        <img class="portada" src=${movies[i].url_foto}>    
+        <h3>${movies[i].titulo}</h3>
+        <p>${movies[i].year}</p>
+        <p>${movies[i].genero}</p>
+    </article>`
+}
+
+allPeliculas.innerHTML = pelicula;
